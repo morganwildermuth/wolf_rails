d9f6e72d582db1b4c6cd734ee4080bccfdfb4087
Morgan Wildermuth <WEF6@Morgans-MacBook-Air.local>
2013-10-10 19:08:15 -0700

    add rails files with preferred testing (capybara, factory_girl, rspec)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WolfRails::Application.config.secret_key_base = '1515eee8bb4db5e626193bf55b3baae4d7376eb8381855a462de6bc74a0d38a61f7e01e8a3b5c72f64b6a4081c4d070b4e163c820743a5acd6748168bbbc0268'
