866854c4fe85983eb36e8985725b56290ec2c2e5
morganwildermuth <morgan.wildermuth@gmail.com>
2013-10-13 16:03:52 -0700

    create session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_username(params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      redirect_to user_path(@user)
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
 end
