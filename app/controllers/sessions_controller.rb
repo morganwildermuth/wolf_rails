c96643145c8c491a88ce6baafa02fce6ebc949bb
morganwildermuth <morgan.wildermuth@gmail.com>
2013-12-20 04:46:17 -0500

    error alert shows when a user does not sign in appropriately

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,12 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
-
   def create
     @user = User.find_by_username(params[:sessions][:username])
     if @user && @user.authenticate(params[:sessions][:password])
       log_in(@user)
       redirect_to user_path(@user)
     else
+      flash[:notice] = "Incorrect username and password combination."
       redirect_to new_session_path
     end
   end
